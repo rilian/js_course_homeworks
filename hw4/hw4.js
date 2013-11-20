46e3b32a8946e3f03f53d7e050cf30565e530264
rilian <dmitriyis@gmail.com>
2013-11-20 20:08:26 +0200

    functions for hw4

---
+++ hw4/hw4.js
@@ -0,0 +1,29 @@
+function getObject(path, object) {
+  var tail = path.split(".");
+  if (tail.length == 1) return object[tail[0]];
+  var attr = tail.shift();
+  return getObject(tail.join("."), object[attr]);
+}
+
+function deepCopy(source) {
+  var o = new Object;
+  for (var property in source) {
+    if (typeof source[property] === "object"){
+      o[property] = deepCopy(source[property]);
+    } else {
+      o[property] = source[property];
+    }
+  }
+  return o;
+};
+
+var getFriends = function(userId) {
+  var user = people.filter(function(el){
+    return (el.id == userId);
+  })[0];
+  if (typeof user == 'undefined') return null;
+  if (user.friends == null) return [];
+  return people.filter(function(el){
+    return (user.friends.indexOf(el.id) != -1);
+  });
+};
